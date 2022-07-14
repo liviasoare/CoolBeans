fed2153cafb08feef50556d238a8348ef14dc2fa
Livia Soare <liv13soare@gmail.com>
2022-07-15 00:06:52 +0300

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Livia Soare on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
