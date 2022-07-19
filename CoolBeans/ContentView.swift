66f2299b4e9eef750cc92ddbeb87a69b9226a4e4
Livia Soare <liv13soare@gmail.com>
2022-07-19 23:59:53 +0300

    GitHub fix

--- CoolBeans/ContentView.swift
+++ CoolBeans/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
     }
     
     
-    var body: some View {
+    var body: some View { //removed MenuView()
         NavigationView{
             List{
                 if history.servings.isEmpty{
